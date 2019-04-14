4ddd8827b30c3c31348a32afbc2e8e4253dfc32f
csibivili <csibi.vili@gmail.com>
2019-04-14 18:52:19 +0200

    Updates

---
+++ precache-manifest.6270eeac3df41b9f616180f25a0b21fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/react-countdown/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "00116c13f419d131f914",
+    "url": "/react-countdown/static/js/runtime~main.19a56f60.js"
+  },
+  {
+    "revision": "4a843155e9a6497441b6",
+    "url": "/react-countdown/static/js/main.2ff18828.chunk.js"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/react-countdown/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "4a843155e9a6497441b6",
+    "url": "/react-countdown/static/css/main.77907ae7.chunk.css"
+  },
+  {
+    "revision": "b63a98474ee25ea0c3ef4612541ebe29",
+    "url": "/react-countdown/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/csibivili/react-countdown/precache-manifest.e3780583f566a7ef8e155123c6f59c5b.js"
+  "/react-countdown/precache-manifest.6270eeac3df41b9f616180f25a0b21fe.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/csibivili/react-countdown/index.html", {
+workbox.routing.registerNavigationRoute("/react-countdown/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
